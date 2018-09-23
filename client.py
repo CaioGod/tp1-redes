0f778ba20de5d1cbce459498624c91c55ed6a2ba
Lucas Furtini <lfurtini@gmail.com>
2018-09-23 14:26:16 -0300

    Servidor checando MD5 e enviando ACKs.

--- client.py
+++ client.py
@@ -5,54 +5,40 @@ import random
 import time
 import math
 import hashlib
-from threading import Thread, Lock
+import threading
 from heapq import heappush, heappop, nsmallest
 
 base9 = 1000000000
 
-readlock = Lock()
+readlock = threading.Lock()
 total_sent = 0
 total_errors = 0
-sliding_window = {}
+acks = {}
+
 
 def build_pack(seq, message):
+
     # Get seconds and nanoseconds
     nano, sec = math.modf(time.time())
 
-    bseq = seq.to_bytes(8, byteorder='big')
-    bsec = int(sec).to_bytes(8, byteorder='big')
-    bnano = int(nano*base9).to_bytes(4, byteorder='big')
-    blen = len(message).to_bytes(2, byteorder='big')
+    bseq = seq.to_bytes(8, byteorder='big', signed=False)
+    bsec = int(sec).to_bytes(8, byteorder='big', signed=False)
+    bnano = int(nano*base9).to_bytes(4, byteorder='big', signed=False)
+    blen = len(message).to_bytes(2, byteorder='big', signed=False)
     bmsg = message.encode('ascii')
-    
+
     return add_checksum(bseq + bsec + bnano + blen + bmsg)
 
 
-def check_ack(ack_package):
-    # TODO
-    # must check ack package
-    return true
-
-def get_index(ack_package):
-    # TODO
-    # must return ack index (seq_number acked)
-    return 1
-
-def is_error():
-    global PERROR
-    rand = random.uniform(0, 1)
-    if rand < PERROR:
-        return True
-    return False
-
-
 def add_checksum(partial):
+
     global total_errors
+
     m = hashlib.md5()
     m.update(partial)
 
-    if is_error(PERROR):
-        total_error += 1
+    if is_error():
+        total_errors += 1
         bchecksum = (random.getrandbits(128)).to_bytes(16, byteorder='big')
     else:
         bchecksum = bytes.fromhex(m.hexdigest())
@@ -60,25 +46,49 @@ def add_checksum(partial):
     return partial + bchecksum
 
 
-def send_thread(udp, server_address, contentArray):
-    global readlock, sliding_window, total_sent
-    while True:
-        readlock.acquire()
-        if sw_begin == len(contentArray):
-            readlock.release()
-            break
-        try:
-            for key, value in sliding_window.items():
-                if value != -1:
-                    package = build_pack(key, contentArray[key].rstrip().replace("\n", ""))
-                    udp.sendto(package, server_address)
-                    total_sent += 1
-        finally:
-            readlock.release()
+def is_error():
+    global PERROR
+    rand = random.uniform(0, 1)
+
+    return rand < PERROR
+
+
+def check_ack(ack_package):
+    # TODO
+    # must check ack package
+    return True
+
+
+def get_index(ack_package):
+
+    # TODO
+    # must return ack index (seq_number acked)
+    return 1
+
+
+def send_thread(udp, server_address, contentList):
+
+    global readlock, acks, total_sent
+
+    readlock.acquire()
+    if sw_begin == len(contentList):
+        readlock.release()
+
+    try:
+        print('Tamanho ACKS: {}'.format(len(acks)))
+        for key, value in acks.items():
+            if value != -1:
+                package = build_pack(key, contentList[key])
+                udp.sendto(package, server_address)
+                total_sent += 1
+    finally:
+        readlock.release()
 
 
 def ack_thread(udp, contentSize):
-    global readlock, sliding_window, total_sent, sw_begin, sw_end 
+
+    global readlock, acks, total_sent, sw_begin, sw_end
+
     acked = []
 
     while True:
@@ -90,35 +100,40 @@ def ack_thread(udp, contentSize):
 
         try:
             ack_pkg, addr = udp.recvfrom(36)
+            seqnumber = 0
+            seqnumber = seqnumber.from_bytes(
+                ack_pkg[:8], byteorder='big', signed=False)
+            print('ACK #: {}'.format(seqnumber))
+
         except socket.timeout:
             continue
 
         # TODO implementar check_package ou similar
-        if not check_package(ack_pkg):
-            continue
+        #if not check_package(ack_pkg):
+        #   continue
 
         # TODO implementar get_index ou similar
         ack_index = get_index(ack_pkg)
-        
+
         readlock.acquire()
         # Verifica se o pacote está dentro da janela
         if ack_index >= sw_begin and ack_index <= sw_end:
             heappush(acked, ack_index)
-            
-            # Colocando -1 nos vetor de sliding_window para não reenviar o mesmo pacote
+
+            # Colocando -1 nos vetor de acks para não reenviar o mesmo pacote
             if sw_begin != nsmallest(1, acked)[0]:
-                sliding_window[ack_index] = -1
-            
+                acks[ack_index] = -1
+
             # Atualiza o limite da janela
             while len(acked) > 0 and sw_begin == nsmallest(1, acked)[0]:
                 heappop(acked)
-                del sliding_window[sw_begin]
+                del acks[sw_begin]
                 sw_begin += 1
-            
+
             # Colocando 0 nos novos pacotes a serem enviados
             while True:
-                if not sw_end in sliding_window: 
-                    sliding_window[sw_end] = 0
+                if not sw_end in acks:
+                    acks[sw_end] = 0
                 if sw_end == contentSize - 1:
                     break
                 if sw_end >= sw_begin + WTX-1:
@@ -128,7 +143,7 @@ def ack_thread(udp, contentSize):
 
 
 def main():
-    global WTX, TOUT, PERROR, sliding_window, sw_begin, sw_end
+    global WTX, TOUT, PERROR, acks, sw_begin, sw_end
 
     # Leitura de argumentos
     start_time = time.time()
@@ -136,6 +151,7 @@ def main():
         print("ERROR: Argumentos invalidos.")
         print("Tente: fileName IP:port Wtx Tout Perror")
         sys.exit()
+
     FILE = sys.argv[1]
     IP, PORT = sys.argv[2].split(":")
     WTX = int(sys.argv[3])
@@ -146,30 +162,39 @@ def main():
     sw_begin = 0
     sw_end = WTX-1
     for v in range(sw_end):
-        sliding_window[v] = 0
+        acks[v] = 0
 
     # Instancia socket UDP
-    server_address = (IP, PORT)
+    server_address = (IP, int(PORT))
     udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
     udp.settimeout(TOUT)
-    
-    # Roubada basica carregando todo o arquivo na memoria, facilita MUITO a vida. 
-    # Index vai ser o seq_number, e o valor a mensagem. (em nenhum lugar é cobrado otimização de memoria)
-    f = open(FILE, 'r')
-    contentArray = f.read().split("\n")
-    f.close()
+
+    # Lista contendo as mensagens lidas do arquivo de LOG
+    contentList = []
+    with open(FILE, 'r') as file:
+        for line in file:
+            line = line.strip('\n')
+            contentList.append(line)
 
     # Cria e starta as threads
-    send = Thread(target=send_thread, args=(udp, server_address, contentArray,))
-    ack = Thread(target=ack_thread, args=(udp, len(contentArray),))
-    send.start()
-    ack.start()
-    send.join()
-    ack.join()
+    try:
+        send = threading.Thread(target=send_thread, args=(
+            udp, server_address, contentList,))
+        ack = threading.Thread(
+            target=ack_thread, args=(udp, len(contentList),))
+        send.start()
+        ack.start()
+        send.join()
+        ack.join()
+
+    except (KeyboardInterrupt, SystemExit):
+        exit()
 
     # Imprime valores finais
-    unique = set(contentArray)
-    print("%d %d %d %.3fs" % ((len(unique)), total_sent,  total_errors,  (time.time() - start_time)))
+    unique = set(contentList)
+    print("%d %d %d %.3fs" % ((len(unique)), total_sent,
+                              total_errors,  (time.time() - start_time)))
+
 
 if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -1,19 +1,27 @@
 
 # /usr/bin/python3
-import socket, sys, re, time, math, hashlib
+import socket
+import sys
+import re
+import time
+import math
+import hashlib
 import random
+from threading import Thread, Lock
 
 base9 = 1000000000
 
 # Recebe numero de sequencia e prob de erro e retorna o pacote ACK
+
+
 def build_ack(seq, perror):
 
     nano, sec = math.modf(time.time())
 
-    bseq  = seq.to_bytes(8, byteoerder='big')
-    bsec  = int(sec).to_bytes(8, byteoerder='big')
-    bnano = int(nano*base9).to_bytes(4, byteoerder='big')
-    
+    bseq = seq.to_bytes(8, byteorder='big')
+    bsec = int(sec).to_bytes(8, byteorder='big')
+    bnano = int(nano*base9).to_bytes(4, byteorder='big')
+
     ack = bseq + bsec + bnano
 
     m = hashlib.md5()
@@ -27,8 +35,9 @@ def build_ack(seq, perror):
 
     return add_checksum(bseq + bsec + bnano + md5)
 
+
 def add_checksum(partial):
-    
+
     m = hashlib.md5()
     m.update(partial)
 
@@ -40,12 +49,62 @@ def add_checksum(partial):
     return partial + bchecksum
 
 # Geracao de erro na mensagem
+
+
 def is_error():
     global PERROR
     rand = random.uniform(0, 1)
-    if rand < PERROR:
-        return True
-    return False
+
+    return (rand < PERROR)
+
+# Thread do usuario
+
+
+def user_thread(udp, outputFile):
+
+    global WTX, PERROR
+    print('user')
+
+    while True:
+        # Tamanho maximo do pacote = 8 + 8 + 4 + 2 + 2^14 + 16
+        data, address = udp.recvfrom(16422)
+
+        print('Origem: {}'.format(address))
+
+        # Verificacao do md5
+        if(check_md5(data)):
+            # Enviar ACK
+            print('Envia ACK')
+            seqnumber = 0
+            seqnumber = seqnumber.from_bytes(
+                data[:8], byteorder='big', signed=False)
+
+            ack = build_ack(seqnumber, PERROR)
+
+            udp.sendto(ack, address)
+
+
+# Retorna True se md5 estiver correto
+def check_md5(data):
+
+    bSeqNumber = data[0:8]
+    bSeconds = data[8:16]
+    bNanoSec = data[16:20]
+    bMessageSize = data[20:22]
+
+    messageSize = 0
+    messageSize = messageSize.from_bytes(
+        data[20:22], byteorder='big', signed=False)
+
+    bMessage = data[22:(22 + messageSize)]
+    md5Recebido = (data[(22 + messageSize):(22 + messageSize + 16)]).hex()
+
+    md5Calculado = hashlib.md5()
+    md5Calculado.update(
+        (bSeqNumber + bSeconds + bNanoSec + bMessageSize + bMessage))
+
+    return md5Recebido == md5Calculado.hexdigest()
+
 
 def main():
     global WTX, PERROR
@@ -55,9 +114,9 @@ def main():
         print("Tente: fileName port Wtx Perror")
         sys.exit()
 
-    FILE   = sys.argv[1]
-    PORT   = int(sys.argv[2])
-    WTX    = int(sys.argv[3])
+    FILE = sys.argv[1]
+    PORT = int(sys.argv[2])
+    WTX = int(sys.argv[3])
     PERROR = float(sys.argv[4])
 
     server_address = ('localhost', PORT)
@@ -65,89 +124,14 @@ def main():
     udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
     udp.bind(server_address)
 
-    while True:
-        data, address = udp.recvfrom(4096)
+    try:
+        user = Thread(target=user_thread, args=(udp, FILE,))
+        user.start()
 
-        print('Destino: {}'.format(address))
-        print(data)
-        print('-------------')
-        print(data[:8])
-        teste = 0
-        print(teste.from_bytes(data[:8], byteorder='big', signed=False))
+    except (KeyboardInterrupt, SystemExit):
+        user.join()
+        exit()
+
 
 if __name__ == '__main__':
     main()
-
-# REFERENCIA: TP3
-
-# # dic global que armazena relaçao tag:client
-# subscriptions = {}
-
-# # verifica se a tag é alphanumérica
-# def isValid(tag):
-#     return tag.isalnum()
-
-# # verifica se a string é uma tag
-# def isTag(s):
-#     return (s == '+' or s == '-' or s == '#')
-
-# # cria uma lista vazia em subscriptions[tag] caso não exista
-# # insere o address dentro dela
-# def insert(address, tag):
-#     if isValid(tag):
-#         if not tag in subscriptions:
-#             subscriptions[tag] = []
-#         if not address in subscriptions[tag]:
-#             subscriptions[tag].append(address)
-
-# # remove endereço do subscriptions[tag] nao faz nada caso não exista
-# def remove(address, tag):
-#     if isValid(tag):
-#         if tag in subscriptions:
-#             if address in subscriptions[tag]: 
-#                 subscriptions[tag].remove(address)
-
-# # verifica quem está escutando a tag, e envia mensgaem para todos
-# def publish(sock, message, tag):
-#     if isValid(tag):
-#         if tag in subscriptions:
-#             for address in subscriptions[tag]:
-#                 sock.sendto(message, address)
-
-# def main():
-#     PORT = int(sys.argv[1])
-#     # Create a UDP socket
-#     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-#     # Bind the socket to the port
-#     server_address = ('localhost', PORT)
-#     print('starting up on {} port {}'.format(*server_address))
-#     sock.bind(server_address)
-
-#     while True:
-#         data, address = sock.recvfrom(4096)
-#         message = data.decode("utf-8")
-
-#         # aqui eu crio uma lista dando split nos espaços e nas tags, mas mantento as tags.
-#         # a mensagem "neymar #ney +bruna" gera tokens = ['neymar', ' ', '#', 'ney', ' ', '+', 'bruna']
-#         tokens = list(filter(None, re.split('(\+|\-|\#| )', message)))
-
-#         # itero os tokens, fazendo as actions na ordem que elas vieram no texto 
-#         # no exemplo acima # vai ser feito antes do +
-#         action = ''
-#         for token in tokens:
-#             if isTag(token):
-#                 action = token
-#             elif action == '+':
-#                 insert(address, token)
-#                 action = ''
-#             elif action == '-':
-#                 remove(address, token)
-#                 action = ''
-#             elif action == '#':
-#                 publish(sock, data, token)
-#                 action = ''
-        
-#         print(subscriptions)
-
-# main()
